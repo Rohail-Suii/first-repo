87e15e6dea990a01526f6db60db295708bbbda12
Rohail <rohailahmed303@gmail.com>
2023-02-21 19:25:50 +0500

    d.adding

---
+++ d.java
@@ -0,0 +1,7 @@
+public class j{
+    public static void main(String args){
+        System.out.println("Assalamu ALikum");
+        System.out.println("Assalamu ALikum");
+        System.out.println("Assalamu ALikum");
+    }
+}
